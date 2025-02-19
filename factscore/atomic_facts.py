a3fdc3cb4972b3ed2f528a190c0d589a3ed76594
ksenmel <ksemelashenko@gmail.com>
2025-02-19 15:45:37 +0300

    add `get_init_atomic_facts_from_sentence` with prompt

--- factscore/atomic_facts.py
+++ factscore/atomic_facts.py
@@ -1,24 +1,43 @@
 from langchain_openai import ChatOpenAI
-import spacy, os, json
+import spacy
 
 
 class AtomicFactGenerator(object):
-    def __init__(self, llm: ChatOpenAI, demon_dir):
+    def __init__(self, llm: ChatOpenAI):
         self.nlp = spacy.load("en_core_web_sm")
-        self.few_shot_path = os.path.join(demon_dir, "demons.json")
         self.llm = llm
-
-        with open(self.few_shot_path, 'r') as f:
-            self.demons = json.load(f)
+        self.is_bio = True
 
     def get_init_atomic_facts_from_sentence(self, sentences):
         atomic_facts = {}
         for sent in sentences:
-            prompt = f""" """
+            prompt = (
+                    """ Please breakdown the following sentence into independent facts: He made his acting debut in the film The Moon is the Sun's Dream (1992), and continued to appear in small and supporting roles throughout the 1990s.
+- He made his acting debut in the film.
+- He made his acting debut in The Moon is the Sun's Dream.
+- The Moon is the Sun's Dream is a film.
+- The Moon is the Sun's Dream was released in 1992.
+- After his acting debut, he appeared in small and supporting roles.
+- After his acting debut, he appeared in small and supporting roles throughout the 1990s.\nHe is also a successful producer and engineer, having worked with a wide variety of artists, including Willie Nelson, Tim McGraw, and Taylor Swift.
+- He is successful.
+- He is a producer.
+- He is a engineer.
+- He has worked with a wide variety of artists.
+- Willie Nelson is an artist.
+- He has worked with Willie Nelson.
+- Tim McGraw is an artist.
+- He has worked with Tim McGraw.
+- Taylor Swift is an artist.
+- He has worked with Taylor Swift.\nPlease breakdown the following sentence into independent facts: """
+                    + sent
+            )
 
             response = self.llm.invoke(prompt)
 
-            facts = [fact.strip() for fact in response.content.split(",") if fact.strip()]
+            facts = [
+                fact.strip() for fact in response.content.split(",") if fact.strip()
+            ]
             atomic_facts[sent] = facts
 
         return atomic_facts
+
