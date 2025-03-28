83cbbaffadc76e29fe2136e2e586ddc8d568cb98
ksenmel <ksemelashenko@gmail.com>
2025-03-28 22:24:09 +0300

    move connection and titles init to `DocDB`

--- factscore/database.py
+++ factscore/database.py
@@ -1,19 +1,60 @@
+import asyncio
 import sqlite3
+import numpy as np
 
-SPECIAL_SEPARATOR = "####SPECIAL####SEPARATOR####"
-MAX_LENGTH = 256
+from rank_bm25 import BM25Okapi
+
+from factscore.api_requests import APIEmbeddingFunction
+from factscore.emb_retrieval import EmbedRetrieval
 
 
-class DocDB(object):
+class DocDB:
     """Sqlite document storage. By default, uses Wikipedia dump from 2023/04/01."""
-    def __init__(self, db_path=None):
-        self.db_path = db_path
-        self.connection = sqlite3.connect(self.db_path)
+    def __init__(self, db_path: str, fais_index: str, ef: APIEmbeddingFunction, table: str):
+        self.connection = sqlite3.connect(db_path)
+        self.ef = ef
 
         cursor = self.connection.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 
         if len(cursor.fetchall()) == 0:
-            assert db_path is not None, f"{self.db_path} is empty"
+            if db_path is not None:
+                Exception(f"{db_path} is empty")
+
+        cursor.execute(f"SELECT title FROM {table}")
+        self.titles = [row[0] for row in cursor.fetchall()]
+
+        self.retriever = EmbedRetrieval(index=fais_index, ef=ef, titles=self.titles, connection=self.connection)
+
+    async def search_text_by_queries(self, queries, k):
+        res = await self.retriever.search(queries=queries, k=k)
+        return res
+
+    async def postprocess_text(self):
+        pass
+
+    def get_bm25_passages(self, topic, question, texts, k):
+        """
+        Returns k passages (parts of the texts) most similar to the topic using bm25
+        """
+        query = topic + " " + question.strip() if topic is not None else question.strip()
+        bm25 = BM25Okapi([text["text"].replace("<s>", "").replace(
+            "</s>", "").split() for text in texts])
+        scores = bm25.get_scores(query.split())
+        indices = np.argsort(-scores)[:k]
+        return [texts[i] for i in indices]
 
 
+if __name__ == "__main__":
+    queries = ["Albert Einstein was born in 14 May 1981"]
+    ef = APIEmbeddingFunction(base_url="https://api.deepinfra.com/v1/openai/embeddings",
+                              model_name="sentence-transformers/all-MiniLM-L6-v2",
+                              dimensions=384)
+
+    db = DocDB(db_path="", fais_index="", ef=ef, table='')
+
+    result = asyncio.run(db.search_text_by_queries(queries=queries, k=5))
+
+    titles, texts = result
+    print(titles)
+    # print(texts)
--- factscore/emb_retrieval.py
+++ factscore/emb_retrieval.py
@@ -1,23 +1,18 @@
-import asyncio
-import os
-import sys
 import faiss
-import sqlite3
 import numpy as np
+
+from sqlite3 import Connection
 
 from factscore.api_requests import APIEmbeddingFunction
 
+
 class EmbedRetrieval:
 
-    def __init__(self, index, data_db, ef: APIEmbeddingFunction):
+    def __init__(self, index, titles: list[str], ef: APIEmbeddingFunction, connection: Connection):
         self.ef = ef
         self.index = faiss.read_index(index)
-
-        # need to move this to DocDB?
-        self.connection = sqlite3.connect(data_db)
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("SELECT title FROM documents")
-        self.titles = [row[0] for row in self.cursor.fetchall()]
+        self.connection = connection
+        self.titles = titles
 
     async def search(self, queries, k: int):
         """
@@ -30,33 +25,15 @@ class EmbedRetrieval:
         k_titles = []
         k_texts = []
 
+        cursor = self.connection.cursor()
         for query, ids in enumerate(indices):
             for idx in ids:
                 title = self.titles[idx]
 
-                self.cursor.execute("SELECT text FROM documents WHERE title = ?", (title,))
-                text = [i[0] for i in self.cursor.fetchall()]
+                cursor.execute("SELECT text FROM documents WHERE title = ?", (title,))
+                text = [i[0] for i in cursor.fetchall()]
 
                 k_titles.append(title)
                 k_texts.append(text)
 
         return k_titles, k_texts
-
-
-
-if __name__ == "__main__":
-    queries = ["Albert Einstein"]
-
-    llm = APIEmbeddingFunction(base_url="https://api.deepinfra.com/v1/openai/embeddings",
-                               model_name="sentence-transformers/all-MiniLM-L6-v2",
-                               dimensions=384)
-
-    retrieval = EmbedRetrieval(index="ivf.index",
-                               data_db="/Users/kseniia/enwiki-20230401.db",
-                               ef=llm)
-
-    result = asyncio.run(retrieval.search(queries=queries, k=5))
-
-    titles, texts = result
-    print(titles)
-    # print(texts)
