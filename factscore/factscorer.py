3633f69a7f3b82e22fa7f0ef984f7851183d4fde
ksenmel <ksemelashenko@gmail.com>
2025-05-02 04:29:27 +0300

    change entities retriever logic

--- factscore/factscorer.py
+++ factscore/factscorer.py
@@ -1,6 +1,7 @@
 import asyncio
 import itertools
 import numpy as np
+import time
 
 from typing import List, Tuple
 
@@ -53,16 +54,19 @@ class FactScorer:
 
         for gen in generations:
 
+            start_time = time.time()
             facts, _ = await self.af_generator.run(gen) 
+            end_time = time.time()
+
+            print(f"Generated atomics for {end_time - start_time} seconds")
 
             gen_atoms = list(itertools.chain.from_iterable(facts.values()))
 
-            if len(gen_atoms) == 0:
-                scores.append(0)
-                decisions.append(None)
-                continue
-
+            start_time = time.time()
             atoms_entities = await self.entities_retriever.run(gen_atoms)
+            end_time = time.time()
+
+            print(f"Retrieved entities for {end_time - start_time} seconds")
 
             generation_decisions = await self.is_supported(atoms_entities, k=k)
     
@@ -101,8 +105,6 @@ class FactScorer:
                 decisions[atom] = is_supported
             else:
                 continue
-        
-        print(decisions)
 
         return decisions
 
@@ -112,40 +114,53 @@ class FactScorer:
         Search relevant titles, texts in db and Returns the retrieval part with appropriate info from wiki for each atomic fact
         """
         prompts = []
-        atoms_entities_in_db = {}
 
-        for atom, ents in atoms_entities.items():
-            titles, texts = await self.db.search_text_by_queries(queries=ents, k=k)
+        ents = list(set(itertools.chain.from_iterable(atoms_entities.values())))
+
+        start_time = time.time()
+        _, texts = await self.db.search_text_by_queries(queries=ents, k=k)
+        end_time = time.time()
+
+        print(f"Searched texts with FAISS in {end_time - start_time} seconds")
+
+        start_time = time.time()
+        for atom in atoms_entities.keys():
             
-            atoms_entities_in_db[atom] = titles 
-
             atom_passages = self.db.get_bm25_passages(fact=atom, texts=texts, n=k)
 
             rag_prompt = f"Task: Answer the question based on the given context.\n\n"
+
             for psg in reversed(atom_passages):
                 context = f"Text: {psg}\n"
                 rag_prompt += context
 
-            rag_prompt += (
-                    f'\n\Question: "{atom.strip()}" True or False? '
-                    "Answer True if the information is supported by the context above and False otherwise.\n"
-                    "Output: "
-                )
-
+            rag_prompt += (f'\n\Question: "{atom.strip()}" True or False? '
+                            "Answer True if the information is supported by the context above and False otherwise.\n"
+                            "Output: "
+                            )
+            
             prompts.append((atom, rag_prompt))
+        end_time = time.time()
 
-        print(atoms_entities_in_db)
+        print(f"Did BM25 in {end_time - start_time} seconds")
+
         return prompts
 
 
 if __name__ == "__main__":
     fs = FactScorer()
-    fs.register_knowledge_source(faiss_index="", data_db="", table_name="")
+    fs.register_knowledge_source(faiss_index="/Users/kseniia/factscore/indexes/all_vecs.index", data_db="/Users/kseniia/enwiki-20230401.db", table_name="documents")
     print("DB registered!")
 
-    gen1 = ["Albert Einstein was born on March 14, 1879, in the German city of Ulm beside the Danube River. His parents, Hermann Einstein and Pauline Koch, were middle-class secular Jews."]
-    gen2 = ["During World War II, Alan Turing worked for the Government Code and Cypher School at Bletchley Park, Britain's codebreaking centre that produced Ultra intelligence."]
+    gen1 = "Albert Einstein was born on March 14, 1879, in the German city of Ulm beside the Danube River. His parents, Hermann Einstein and Pauline Koch, were middle-class secular Jews."
+    gen2 = "During World War II, Alan Turing worked for the Government Code and Cypher School at Bletchley Park, Britain's codebreaking centre that produced Ultra intelligence."
+    gen3 = "Elvis Presley (1935–1977) was an American singer and actor widely regarded as the 'King of Rock and Roll'. "
 
-    res = asyncio.run(fs.get_score(generations=gen2, k=2))
+    gen4 = "Elvis Presley (1935–1977) was an American singer and actor widely regarded as the “King of Rock and Roll.” Elvis Presley is one of the most significant cultural icons of the 20th century and helped revolutionize popular music with his energetic performance style, powerful voice, and charismatic stage presence. Born on January 8, 1935, in Tupelo, Mississippi, Elvis Presley moved to Memphis, Tennessee, as a teenager. In 1954, Elvis Presley began his music career with Sun Records, blending country, blues, and gospel influences into a new sound that would come to be known as rock and roll. His breakout hit, “Heartbreak Hotel,” in 1956 launched him to national fame. In addition to music, Elvis Presley had a successful film career, starring in over 30 movies throughout the 1960s. After a brief break from live performances, Elvis Presley made a legendary comeback with a 1968 TV special and went on to perform sold-out shows in Las Vegas during the 1970s. Despite his success, Elvis Presley struggled with health problems and prescription drug dependence. Elvis Presley died of a heart attack on August 16, 1977, at his home, Graceland, in Memphis, at age 42."
+
+    start_time = time.time()
+    res = asyncio.run(fs.get_score(generations=[gen1], k=2))
+    end_time = time.time()
 
     print(res)
+    print(f"Processed whole program for {end_time - start_time} seconds")
