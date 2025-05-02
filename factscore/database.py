027e733b5a63bb593a8b523b5d35945aff6267d4
ksenmel <ksemelashenko@gmail.com>
2025-05-02 21:03:54 +0300

    make factscore wayyyy faster

--- factscore/database.py
+++ factscore/database.py
@@ -6,8 +6,6 @@ from rank_bm25 import BM25Okapi
 from factscore.api_requests import APIEmbeddingFunction
 from factscore.emb_retrieval import EmbedRetrieval
 
-SPECIAL_SEPARATOR = "####SPECIAL####SEPARATOR####"
-
 
 class DocDB:
     """Sqlite document storage. By default, uses Wikipedia dump from 2023/04/01"""
@@ -32,33 +30,6 @@ class DocDB:
             index=faiss_index, ef=ef, titles=self.titles, connection=self.connection
         )
 
-        self.segmenter = Segmenter(language="en")
-
     async def search_text_by_queries(self, queries, k):
         res = await self.retriever.search(queries=queries, k=k)
         return res
-
-    def get_bm25_passages(self, fact, texts, n):
-        """
-        Returns k passages most similar to the fact using BM25
-        """
-        query = fact
-        passages = []
-
-        for text in texts:
-            corpus = self.segmenter.segment(text[0])
-
-            tokenized_corpus = []
-            for doc in corpus:
-                doc_tokens = doc.split()
-                tokenized_corpus.append(doc_tokens)
-
-            bm25 = BM25Okapi(tokenized_corpus)
-
-            tokenized_query = query.split(" ")
-
-            doc = bm25.get_top_n(tokenized_query, corpus, n)
-
-            passages.append(doc)
-
-        return passages
--- factscore/factscorer.py
+++ factscore/factscorer.py
@@ -3,6 +3,7 @@ import itertools
 import numpy as np
 import time
 
+from rank_bm25 import BM25Okapi
 from typing import List, Tuple
 
 from factscore.api_requests import APICompletions
@@ -28,6 +29,8 @@ class FactScorer:
         )
         self.af_generator = AtomicFactGenerator(llm=self.completions_lm)
         self.entities_retriever = EntitiesRetriever(llm=self.completions_lm)
+
+        self.segmenter = self.af_generator.segmenter
 
     def register_knowledge_source(
         self,
@@ -124,9 +127,20 @@ class FactScorer:
         print(f"Searched texts with FAISS in {end_time - start_time} seconds")
 
         start_time = time.time()
+        corpus = []
+        for text in texts:
+            sents = self.segmenter.segment(text[0])
+            corpus.extend(sents)
+
+        print(corpus)
+
+        tokenized_corpus = [doc.split(" ") for doc in corpus]
+
+        bm25 = BM25Okapi(tokenized_corpus)
+
         for atom in atoms_entities.keys():
-            
-            atom_passages = self.db.get_bm25_passages(fact=atom, texts=texts, n=k)
+
+            atom_passages = bm25.get_top_n(atom.split(" "), corpus, n=k)
 
             rag_prompt = f"Task: Answer the question based on the given context.\n\n"
 
@@ -140,6 +154,7 @@ class FactScorer:
                             )
             
             prompts.append((atom, rag_prompt))
+
         end_time = time.time()
 
         print(f"Did BM25 in {end_time - start_time} seconds")
@@ -159,7 +174,7 @@ if __name__ == "__main__":
     gen4 = "Elvis Presley (1935–1977) was an American singer and actor widely regarded as the “King of Rock and Roll.” Elvis Presley is one of the most significant cultural icons of the 20th century and helped revolutionize popular music with his energetic performance style, powerful voice, and charismatic stage presence. Born on January 8, 1935, in Tupelo, Mississippi, Elvis Presley moved to Memphis, Tennessee, as a teenager. In 1954, Elvis Presley began his music career with Sun Records, blending country, blues, and gospel influences into a new sound that would come to be known as rock and roll. His breakout hit, “Heartbreak Hotel,” in 1956 launched him to national fame. In addition to music, Elvis Presley had a successful film career, starring in over 30 movies throughout the 1960s. After a brief break from live performances, Elvis Presley made a legendary comeback with a 1968 TV special and went on to perform sold-out shows in Las Vegas during the 1970s. Despite his success, Elvis Presley struggled with health problems and prescription drug dependence. Elvis Presley died of a heart attack on August 16, 1977, at his home, Graceland, in Memphis, at age 42."
 
     start_time = time.time()
-    res = asyncio.run(fs.get_score(generations=[gen1], k=2))
+    res = asyncio.run(fs.get_score(generations=[gen4], k=2))
     end_time = time.time()
 
     print(res)
